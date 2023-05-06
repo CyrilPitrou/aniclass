b9015fc79f554f687d6e6785b6fdf16cbbdf5536
Cyril Pitrou <cyril.pitrou@gmail.com>
2023-05-07 01:04:35 +0200

    Merged manually the Total Angular Momentum method which was implemented in a separate branch in 2020. The test_class.py has been modified accordingly.
    It passes all the nosetests
    
    Results are slightly different from the devel branch but I have checked that this is due to the previously incorrect TCA for tensor modes.
    I have checked on plots that the new TCA is correct since it leads to a smooth transition when TCA is turned off, whereas it was
    not the case before.

--- source/input.c
+++ source/input.c
@@ -2307,6 +2307,22 @@ int input_read_parameters_general(struct file_content * pfc,
     class_read_double("bbn_alpha_sensitivity",pth->bbn_alpha_sensitivity);
   }
 
+  /** 11) version of Boltzmann equation hierarchy */
+
+  class_call(parser_read_string(pfc,"hierarchy",&string1,&flag1,errmsg),
+             errmsg,
+             errmsg);
+
+  if (flag1 == _TRUE_) {
+
+    if (strstr(string1,"optimal") != NULL) {
+      ppt->hierarchy = optimal;
+    }
+    else if ((strstr(string1,"tam") != NULL) || (strstr(string1,"TAM") != NULL)) {
+      ppt->hierarchy = tam;
+    }
+  }
+  
   return _SUCCESS_;
 
 }
@@ -5734,6 +5750,9 @@ int input_default_params(struct background *pba,
   pba->varconst_me = 1.;
   pth->bbn_alpha_sensitivity = 1.;
   pba->varconst_transition_redshift = 50.;
+
+  /** 11 hierarchy type */
+  ppt->hierarchy = optimal;
 
   /**
    * Default to input_read_parameters_species
